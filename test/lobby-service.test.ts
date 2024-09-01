be7a2ce0b7e9361c81b4c2217f13d63fd54ec7cd
Alexander Kaserbacher <alex.kaserbacher@icloud.com>
2024-09-01 09:51:44 +0200

    Initial commit

---
+++ test/lobby-service.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as LobbyService from '../lib/lobby-service-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/lobby-service-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new LobbyService.LobbyServiceStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
