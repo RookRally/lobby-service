d9af802c74ae2ece35e72514020d8a52de46f35d
Alexander Kaserbacher <alex.kaserbacher@icloud.com>
2024-09-01 11:08:31 +0200

    add API Gateway and User Pool auth

--- lib/lobby-service-stack.ts
+++ lib/lobby-service-stack.ts
@@ -1,16 +1,36 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as apigateway from "aws-cdk-lib/aws-apigateway";
+import * as cognito from "aws-cdk-lib/aws-cognito";
 
 export class LobbyServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const api = new apigateway.RestApi(this, "lobbyApi");
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'LobbyServiceQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const userPool = cognito.UserPool.fromUserPoolId(
+      this,
+      "rookrallyUserPool",
+      cdk.Fn.importValue("rookrallyUserPoolId"),
+    );
+
+    const auth = new apigateway.CognitoUserPoolsAuthorizer(
+      this,
+      "lobbyAuthorizer",
+      {
+        cognitoUserPools: [userPool],
+      },
+    );
+
+    const candidates = api.root.addResource("candidates");
+    candidates.addMethod(
+      "POST",
+      new apigateway.HttpIntegration("http://amazon.com"),
+      {
+        authorizer: auth,
+        authorizationType: apigateway.AuthorizationType.COGNITO,
+      },
+    );
   }
 }
